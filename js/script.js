8b22896569e4d4e6d5927e7846298fe7d44583f4
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-04 15:05:58 +0000

    Started JS file by declaring variables

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022 Maria Goemans All rights reserved
+//
+// Created by: Maria G
+// Created on: May 4, 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+function infoSent() {
+  //declaring & defining variables
+  let userAge = parseInt(document.getElementById("user-age").value);
+  let form = document.getElementById("day").value;
+}
