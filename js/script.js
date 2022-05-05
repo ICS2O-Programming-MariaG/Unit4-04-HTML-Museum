ba553b06f049de77082888e46be0dafb65e42b14
MariaGoemans1 <99274593+MariaGoemans@users.noreply.github.com>
2022-05-05 19:30:37 +0000

    Set constants for age thresholds

--- js/script.js
+++ js/script.js
@@ -12,12 +12,17 @@ function infoSent() {
   //declaring & defining variables
   let userAge = parseInt(document.getElementById("user-age").value);
   let day = document.getElementById("day").value;
+  //setting constants for age thresholds
+  const CHILD_FREE = 5;
+  const ELDERLY_FREE = 95;
+  const STUDENT_MINIMUM = 12;
+  const STUDENT_MAXIMUM = 21;
   //using compound if statements to determine cost
-  if ((userAge < 5) || (userAge > 95)) {
+  if ((userAge < CHILD_FREE) || (userAge > ELDERLY_FREE)) {
     cost = "You get in for free!";
   }
   //Added extra brackets around all conditions to fix error - the computer was not processing the || (or) symbol when it was outside of brackets
-  else if (((userAge >= 12) && (userAge <= 21)) || ((day == "Thursday") || (day == "Tuesday"))) {
+  else if (((userAge >= STUDENT_MINIMUM) && (userAge <= STUDENT_MAXIMUM)) || ((day == "Thursday") || (day == "Tuesday"))) {
     cost = "You get a discount with student pricing!";
   }
   else {
